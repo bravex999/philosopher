daafb2f1c8a55f856c0a687c71f0554f835c63f9
naranjox <cdny195@gmail.com>
2025-09-21 15:58:16 +0200

    punto h con librerias listo.

---
+++ philo.h
@@ -0,0 +1,12 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h> ç
+
+#endif
