933c849738f898cb4b25c8f2b8c419b713343ee3
naranjox <cdny195@gmail.com>
2025-09-22 11:47:01 +0200

    main mejorado

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include "philo.h"
 
+static void print_number_args_error(); 
+
 int main (int argc, char **argv)
 {
 	(void)argv;
 
 	if(argc != 5 && argc != 6)
 	{
-		printf("%s","Usage: ./philo "
-		"number_of_philosophers time_to_die time_to_eat"
-		" time_to_sleep optional:"
-		"[number_of_times_each_philosopher_must_eat] \n");
+		print_number_args_error();
 		return (1);
 	}
 	return (0);
 }
+
+static void print_number_args_error()
+{
+	write(2, "Usage: ./philo number_of_philosophers time_to_die time_to_eat "
+      "time_to_sleep [number_of_times_each_philosopher_must_eat]\n",
+      sizeof("Usage: ./philo number_of_philosophers time_to_die time_to_eat "
+             "time_to_sleep [number_of_times_each_philosopher_must_eat]\n") - 1);
+
+}	
+
