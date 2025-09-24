91142dad797dd8947e1711a73b9ae566259cf660
Bravex <cdny1987@gmail.com>
2025-09-24 19:08:37 +0200

    rellena t_config, falta inicilizar las otras dos structs

--- init_setup.c
+++ init_setup.c
@@ -1,6 +1,20 @@
 #include "philo.h"
 
-int init_structs(*t_shared shared, *t_config config, int *temp)
+int init_structs(t_shared *shared, t_config *config, int *temp)
+{
+	void(shared);
+
+	config->n = temp[0];
+	config->t_die_ms = temp[1];
+	config->t_eat_ms = temp[2];
+	config->t_sleep_ms = temp[3];
+	if(temp[4])
+		config->must_eat = temp[4];
+}	
+
+
+
+
 		
 
 
