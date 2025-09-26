26c1b9d4234df5eb46a5ecce673a3a44adbceac3
Bravex <cdny1987@gmail.com>
2025-09-26 15:18:26 +0200

    init mutexex listinit mutexex list

---
+++ mutex_init.c
@@ -0,0 +1,40 @@
+#include "philo.h"
+
+static void	destroy_forks(t_shared *shared, int limit)
+{
+	int	j;
+
+	j = 0;
+	while (j < limit)
+	{
+		pthread_mutex_destroy(&shared->forks[j].mtx);
+		j++;
+	}
+}
+
+int	init_mutexes(t_shared *shared)
+{
+	int	i;
+
+	if (pthread_mutex_init(&shared->mutex_print, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&shared->mutex_state, NULL) != 0)
+	{
+		pthread_mutex_destroy(&shared->mutex_print);
+		return (1);
+	}
+	i = 0;
+	while (i < shared->cfg.n)
+	{
+		if (pthread_mutex_init(&shared->forks[i].mtx, NULL) != 0)
+		{
+			destroy_forks(shared, i);
+			pthread_mutex_destroy(&shared->mutex_state);
+			pthread_mutex_destroy(&shared->mutex_print);
+			return (1);
+		}
+		shared->forks[i].id = i;
+		i++;
+	}
+	return (0);
+}
