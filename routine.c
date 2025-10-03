65bad2cb6a1b1d03915db572ff6368dfa40d5ae8
naranjox <cdny195@gmail.com>
2025-10-03 17:32:23 +0200

    norminiette listo

--- routine.c
+++ routine.c
@@ -9,21 +9,12 @@
 /*   Updated: 2025/10/01 18:46:03 by chnaranj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "philo.h"
 
-static int	try_take_forks(t_philos *p)
+static int	try_take_forks_help(t_philos *p)
 {
 	int	stop;
 
-	pthread_mutex_lock(&p->shared->mutex_state);
-	stop = p->shared->stop;
-	pthread_mutex_unlock(&p->shared->mutex_state);
-	if (stop)
-		return (1);
-	print_event(p->shared, LOG_THINKING, p->id);
-	pthread_mutex_lock(&p->first->mtx);
-	print_event(p->shared, LOG_TAKEN_FORK, p->id);
 	pthread_mutex_lock(&p->shared->mutex_state);
 	stop = p->shared->stop;
 	pthread_mutex_unlock(&p->shared->mutex_state);
@@ -42,6 +33,45 @@ static int	try_take_forks(t_philos *p)
 	return (0);
 }
 
+static int	try_take_forks(t_philos *p)
+{
+	int	stop;
+
+	pthread_mutex_lock(&p->shared->mutex_state);
+	stop = p->shared->stop;
+	pthread_mutex_unlock(&p->shared->mutex_state);
+	if (stop)
+		return (1);
+	print_event(p->shared, LOG_THINKING, p->id);
+	pthread_mutex_lock(&p->first->mtx);
+	print_event(p->shared, LOG_TAKEN_FORK, p->id);
+	return (try_take_forks_help(p));
+}
+
+static void	philo_routine_help(t_philos *p)
+{
+	t_ms	think;
+
+	pthread_mutex_lock(&p->shared->mutex_state);
+	p->last_meal_ms = now_ms(p->shared);
+	p->meals++;
+	pthread_mutex_unlock(&p->shared->mutex_state);
+	print_event(p->shared, LOG_EATING, p->id);
+	super_sleep(p->shared->cfg.t_eat_ms, p->shared);
+	pthread_mutex_unlock(&p->first->mtx);
+	pthread_mutex_unlock(&p->second->mtx);
+	print_event(p->shared, LOG_SLEEPING, p->id);
+	super_sleep(p->shared->cfg.t_sleep_ms, p->shared);
+	if ((p->shared->cfg.n % 2) == 1)
+	{
+		think = p->shared->cfg.t_eat_ms - p->shared->cfg.t_sleep_ms;
+		if (think < 1)
+			think = 1;
+		super_sleep(think, p->shared);
+	}
+	usleep(2000);
+}
+
 void	*philo_routine(void *arg)
 {
 	t_philos	*p;
@@ -53,24 +83,7 @@ void	*philo_routine(void *arg)
 	{
 		if (try_take_forks(p))
 			break ;
-		pthread_mutex_lock(&p->shared->mutex_state);
-		p->last_meal_ms = now_ms(p->shared);
-		p->meals++;
-		pthread_mutex_unlock(&p->shared->mutex_state);
-		print_event(p->shared, LOG_EATING, p->id);
-		super_sleep(p->shared->cfg.t_eat_ms, p->shared);
-		pthread_mutex_unlock(&p->first->mtx);
-		pthread_mutex_unlock(&p->second->mtx);
-		print_event(p->shared, LOG_SLEEPING, p->id);
-		super_sleep(p->shared->cfg.t_sleep_ms, p->shared);
-		if ((p->shared->cfg.n % 2) == 1)
-		{
-			t_ms think = p->shared->cfg.t_eat_ms - p->shared->cfg.t_sleep_ms;
-		if (think < 1)
-			think = 1;
-		super_sleep(think, p->shared);
-		}
-		usleep(2000);
+		philo_routine_help(p);
 	}
 	return (NULL);
 }
